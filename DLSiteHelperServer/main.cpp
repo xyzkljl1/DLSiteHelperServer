a6a67abe006de48c0ecd2695e4f6d248e956c976
xyzkljl1 <xyzkljl1@yahoo.com>
2020-11-10 16:44:05 +0800

    增加启动参数

--- DLSiteHelperServer/main.cpp
+++ DLSiteHelperServer/main.cpp
@@ -5,7 +5,12 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	DBProxyServer server;
-	if (argv[1] == "-w")
+	std::set<std::string> args;
+	for (int i = 1; i < argc; ++i)
+		args.insert(argv[i]);
+	if (args.count("-u"))
+		setvbuf(stdout, NULL, _IONBF, 0);
+	if (args.count("-w"))
 	{
 		MyFakeWindow window;
 		QObject::connect(&window, &MyFakeWindow::signalClose, &a, &QApplication::quit);
