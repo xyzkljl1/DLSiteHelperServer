5e8b9ea73143acd21e0f2a70dd3774522fd6e08b
xyzkljl1 <xyzkljl1@yahoo.com>
2020-11-10 14:00:24 +0800

    增加不显示窗口的选项

--- DLSiteHelperServer/main.cpp
+++ DLSiteHelperServer/main.cpp
@@ -5,7 +5,11 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	DBProxyServer server;
-	MyFakeWindow window;
-	QObject::connect(&window, &MyFakeWindow::signalClose, &a, &QApplication::quit);
-    return a.exec();
+	if (argv[1] == "-w")
+	{
+		MyFakeWindow window;
+		QObject::connect(&window, &MyFakeWindow::signalClose, &a, &QApplication::quit);
+		return a.exec();
+	}
+	return a.exec();
 }
